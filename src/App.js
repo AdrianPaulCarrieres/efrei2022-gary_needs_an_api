cc98ba6fd7e2d1cff9e6f4e40dfef7c0cb22f6f1
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2022-03-15 14:01:30 +0100

    css

--- src/App.js
+++ src/App.js
@@ -20,18 +20,18 @@ function App() {
   }, [quotes]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Gary a besoin d'une API
-      </h1>
-      <div>
-        <button onClick={() => setRandomQuote()}>Get new quote!</button>
-      </div>
-      <div>
-        <Suspense fallback={<div>Chargement...</div>}>
-          {(quotes.length > 0 && <Item quote={quotes[randomIndex]} />) || <div>Chargement...</div>}
-        </Suspense>
-      </div>
+    <div className="min-h-screen flex flex-col justify-center">
+        <h1 className="text-3xl font-bold mx-auto text-white">
+          Gary a besoin d'une API
+        </h1>
+        <div className='mx-auto rounded-full p-2 text-white my-5 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300'>
+          <button onClick={() => setRandomQuote()}>Get new quote!</button>
+        </div>
+        <div className='mx-auto'>
+          <Suspense fallback={<div>Chargement...</div>}>
+            {(quotes.length > 0 && <Item quote={quotes[randomIndex]} />) || <div>Chargement...</div>}
+          </Suspense>
+        </div>
     </div>
   );
 
