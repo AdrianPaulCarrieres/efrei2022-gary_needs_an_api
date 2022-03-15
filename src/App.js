a010b85daacbc33a68290e3c9da0c5d1fb75d69c
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2022-03-15 12:51:48 +0100

    Local storage (#2)
    * localStorage
    
    * get api
    
    * bookmarks
    
    * fix class to className
    
    * YAY

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import React, { useState, useEffect, Suspense } from 'react';
+const List = React.lazy(() => import('./components/List'));
+const Item = React.lazy(() => import('./components/Item'));
 
 function App() {
+
+  const [quotes, setQuotes] = useState([]);
+  const [randomIndex, setRandomIndex] = useState(-1);
+
+  //fetch quotes from API
+  useEffect(() => {
+    fetch('https://finalspaceapi.com/api/v0/quote/')
+      .then(response => response.json())
+      .then(data => setQuotes(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setRandomIndex(randomIndex);
+  }, [quotes]);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Gary a besoin d'une API
+      </h1>
+      <div>
+        <Suspense fallback={<div>Chargement...</div>}>
+          {(quotes.length > 0 && <Item quote={quotes[randomIndex]} />) || <div>Chargement...</div>}
+        </Suspense>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+import Item from "./Item";
+
+function List(props) {
+    return (
+        <div className="List">
+            <ul>
+                {props.quotes.map(quote => (
+                    <li>
+                        <Item quote={quote} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default List;
