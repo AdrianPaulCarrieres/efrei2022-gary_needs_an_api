5512aaafed07e030842bfe29db1b1ff5ee48b5b0
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2022-03-16 12:49:38 +0100

    better style

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getElement, setElement } from '../services';
+import { Link } from "react-router-dom";
 import Item from "./Item";
 
 function Bookmarks() {
@@ -22,13 +23,14 @@ function Bookmarks() {
 
     return (
         <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-3">
+            <div className="relative grid grid-cols-1 md:grid-cols-2 grid-rows-3">
+            <Link to="/bookmarks" className="fixed top-0 left-2 rounded-lg p-2 text-white my-5 transition ease-in-out delay-150 bg-blue-500 hover:-translate-x-1 hover:bg-blue-400 duration-300">Return</Link>
                 {quotes.map(quote => (
                     <div className="flex flex-col">
                         <Item quote={quote} />
                         <div className="mx-auto">
                             <button
-                                className="rounded-lg border p-2 border-red-600 text-red-600"
+                                className="rounded-lg border p-2 border-red-600 text-red-600 transition ease-in-out delay-150 hover:bg-red-600 hover:text-white duration-300"
                                 onClick={() => {
                                     const newBookmarks = bookmarks.filter(i => i !== quote.id);
                                     setElement('bookmarks', newBookmarks);
@@ -39,6 +41,9 @@ function Bookmarks() {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="flex justify-end">
+            <Link to="/bookmarks" className="mx-5 rounded-lg p-2 text-white my-5 transition ease-in-out delay-150 bg-blue-500 hover:-translate-x-5 hover:bg-blue-400 duration-300">Home page</Link>
             </div>
         </div>
     )
